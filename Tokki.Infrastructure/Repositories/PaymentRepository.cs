64d0302d9f67aa827a18e74614844d12dd066204
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-04 16:10:43 +0700

    feat: create paymentRepository

---
+++ Tokki.Infrastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,22 @@
+﻿using Tokki.Application.IRepositories;
+using Tokki.Domain.Entities;
+
+namespace Tokki.Infrastructure.Repositories
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+        private static readonly List<Payment> _mockDb = new();
+
+        public Task AddAsync(Payment payment)
+        {
+            _mockDb.Add(payment);
+            return Task.CompletedTask; 
+        }
+
+        public Task<Payment?> GetByIdAsync(string id)
+        {
+            var payment = _mockDb.FirstOrDefault(p => p.Id == id);
+            return Task.FromResult(payment);
+        }
+    }
+}
