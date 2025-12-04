fdf6487b04da233ddec798aa46f5a6cc10165a1e
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-04 15:59:00 +0700

    feate: create IPaymentRepository

---
+++ Tokki.Application/IRepositories/IPaymentRepository.cs
@@ -0,0 +1,10 @@
+﻿using Tokki.Domain.Entities;
+
+namespace Tokki.Application.IRepositories
+{
+    public interface IPaymentRepository
+    {
+        Task AddAsync(Payment payment);
+        Task<Payment?> GetByIdAsync(string id);
+    }
+}
