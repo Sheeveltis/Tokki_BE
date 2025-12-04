6bf269a52d71b32aae0fb0406dabfff42b05764b
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-04 22:55:38 +0700

    refactor: add getPaymentById

--- Tokki.WebAPI/Controllers/PaymentController.cs
+++ Tokki.WebAPI/Controllers/PaymentController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Tokki.Application.UseCases.Payments.Commands.CreatePayment;
+using Tokki.Application.UseCases.Payments.Queries.GetPaymentById;
 
 namespace Tokki.WebAPI.Controllers
 {
@@ -19,6 +20,14 @@ namespace Tokki.WebAPI.Controllers
         public async Task<IActionResult> Create([FromBody] CreatePaymentCommand command)
         {
             var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var query = new GetPaymentByIdQuery { Id = id };
+            var result = await _sender.Send(query);
             return StatusCode(result.StatusCode, result);
         }
     }
