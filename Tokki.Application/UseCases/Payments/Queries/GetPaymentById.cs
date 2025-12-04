5f693afcfc0b0892b4b33833cf7c74b3c6e6229b
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-04 22:53:43 +0700

    feat: create getPaymentById

---
+++ Tokki.Application/UseCases/Payments/Queries/GetPaymentById.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+using Tokki.Domain.Entities;
+
+namespace Tokki.Application.UseCases.Payments.Queries.GetPaymentById
+{
+    public class GetPaymentByIdQuery : IRequest<OperationResult<Payment>>
+    {
+        public string Id { get; set; }
+    }
+}
