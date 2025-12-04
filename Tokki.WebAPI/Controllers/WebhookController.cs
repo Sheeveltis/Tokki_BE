ba980c22241884f5d505fc1ee670e2eb03ce3ab9
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-04 22:10:09 +0700

    feat: create webhookController

---
+++ Tokki.WebAPI/Controllers/WebhookController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Tokki.Application.UseCases.Payments.Commands.ProcessWebhook;
+
+namespace Tokki.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public WebhookController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("sepay")]
+        public async Task<IActionResult> ReceiveSePayWebhook([FromBody] SePayWebhookData data)
+        {
+            var command = new ProcessWebhookCommand { Data = data };
+            var result = await _sender.Send(command);
+
+            return Ok(new { success = true, message = result.Message });
+        }
+    }
+}
