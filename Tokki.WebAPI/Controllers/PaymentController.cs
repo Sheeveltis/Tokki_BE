17b42a2281f22931cbe755d835faf4d673a30e81
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-04 16:23:04 +0700

    feat: create paymentController

---
+++ Tokki.WebAPI/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Tokki.Application.UseCases.Payments.Commands.CreatePayment;
+
+namespace Tokki.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public PaymentController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreatePaymentCommand command)
+        {
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
