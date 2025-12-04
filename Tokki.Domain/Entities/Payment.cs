e2885b9e528f1c8afaf27375b3a75a0af4f5b605
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-04 15:57:56 +0700

    feat: create Payment entities

---
+++ Tokki.Domain/Entities/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tokki.Domain.Enums;
+
+namespace Tokki.Domain.Entities
+{
+    public class Payment
+    {
+        public string Id { get; set; } = string.Empty; 
+        public string UserId { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public string Description { get; set; } = string.Empty; 
+        public PaymentStatus Status { get; set; } = PaymentStatus.Pending;
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        public string? PaymentUrl { get; set; }
+    }
+}
