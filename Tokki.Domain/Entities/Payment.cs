2a2cf3cc097eef67ecd330369a351d26ad1c598e
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-04 22:01:17 +0700

    refactor: update payment entity

--- Tokki.Domain/Entities/Payment.cs
+++ Tokki.Domain/Entities/Payment.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using Tokki.Domain.Enums;
 
 namespace Tokki.Domain.Entities
@@ -11,6 +12,10 @@ namespace Tokki.Domain.Entities
         public string Description { get; set; } = string.Empty; 
         public PaymentStatus Status { get; set; } = PaymentStatus.Pending;
         public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset? PaidAt { get; set; }
+        public int? TransactionId { get; set; }
+        [ForeignKey("TransactionId")]
+        public virtual Transaction? Transaction { get; set; }
         //public string? PaymentUrl { get; set; }
     }
 }
