e485af8d35147452a72786e5737496682585a236
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-07 12:00:10 +0700

    feat: create GetPaymentQrQuery

---
+++ Tokki.Application/UseCases/Payments/Queries/GetPaymentQr/GetPaymentQrQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+
+namespace Tokki.Application.UseCases.Payments.Queries.GetPaymentQr
+{
+    public class GetPaymentQrQuery : IRequest<OperationResult<string>>
+    {
+        public string PaymentId { get; set; }
+
+        public GetPaymentQrQuery(string paymentId)
+        {
+            PaymentId = paymentId;
+        }
+    }
+}
