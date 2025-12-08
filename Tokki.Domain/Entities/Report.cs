9368395693d016824afe437b8855d47fcf556e10
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-08 15:48:57 +0700

    feat: create reportEntity

---
+++ Tokki.Domain/Entities/Report.cs
@@ -0,0 +1,16 @@
+﻿namespace Tokki.Domain.Entities
+{
+    public class Report
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; } 
+        public string Description { get; set; }
+        public string? ImageUrl { get; set; } 
+        public string? TargetUrl { get; set; } 
+        public ReportStatus Status { get; set; } = ReportStatus.Pending;
+        public string? AdminReply { get; set; } 
+        public bool UserHasRead { get; set; } = true; 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? ResolvedAt { get; set; }
+    }
+}
