85a082d3f8a8b772ddba1d542ae2a38705d88ce6
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-08 15:51:02 +0700

    feat: create IReportRepository

---
+++ Tokki.Application/IRepositories/IReportRepository.cs
@@ -0,0 +1,15 @@
+﻿using Tokki.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tokki.Application.IRepositories
+{
+    public interface IReportRepository
+    {
+        Task<Report> AddAsync(Report report);
+        Task<Report?> GetByIdAsync(Guid id);
+        Task<List<Report>> GetByUserIdAsync(string userId);
+        Task<List<Report>> GetUnreadResolvedReportsAsync(string userId); 
+        Task UpdateAsync(Report report);
+    }
+}
