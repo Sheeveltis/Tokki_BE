35ba8510b8357bd95db3798c7c80ae0467067204
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-08 16:03:25 +0700

    feat: create GetReportNotifications

---
+++ Tokki.Application/UseCases/Reports/Commands/CreateReport/CreateReportHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+using Tokki.Application.IRepositories;
+using Tokki.Application.IServices; 
+using Tokki.Domain.Entities;
+
+namespace Tokki.Application.UseCases.Reports.Commands.CreateReport
+{
+    public class CreateReportHandler : IRequestHandler<CreateReportCommand, OperationResult<string>>
+    {
+        private readonly IReportRepository _reportRepository;
+        private readonly IIdGeneratorService _idGenerator;
+
+        public CreateReportHandler(IReportRepository reportRepository, IIdGeneratorService idGenerator)
+        {
+            _reportRepository = reportRepository;
+            _idGenerator = idGenerator;
+        }
+
+        public async Task<OperationResult<string>> Handle(CreateReportCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var report = new Report
+                {
+                    Id = _idGenerator.GenerateCustom(21), 
+                    UserId = request.UserId,
+                    Description = request.Description,
+                    ImageUrl = request.ImageUrl,
+                    TargetUrl = request.TargetUrl,
+                    Status = 0, 
+                    UserHasRead = true, 
+                    CreatedAt = DateTime.UtcNow
+                };
+
+                await _reportRepository.AddAsync(report);
+                return OperationResult<string>.Success(report.Id);
+            }
+            catch (Exception ex)
+            {
+                return OperationResult<string>.Failure("Lỗi tạo báo cáo: " + ex.Message);
+            }
+        }
+    }
+}
