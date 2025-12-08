0f93cc4970eac65156ce47c7164cf5aa862df007
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-08 16:46:49 +0700

    feat: create updateReportStatus

---
+++ Tokki.Application/UseCases/Reports/Commands/UpdateReportStatus/UpdateReportStatusCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+using Tokki.Domain.Entities;
+using Tokki.Domain.Enums; 
+namespace Tokki.Application.UseCases.Reports.Commands.UpdateReportStatus
+{
+    public class UpdateReportStatusCommand : IRequest<OperationResult<bool>>
+    {
+        public string ReportId { get; set; } 
+
+        public ReportStatus NewStatus { get; set; }
+
+        public string? AdminReply { get; set; }
+    }
+}
---
+++ Tokki.Application/UseCases/Reports/Commands/UpdateReportStatus/UpdateReportStatusHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+using Tokki.Application.IRepositories;
+using Tokki.Domain.Entities;
+
+namespace Tokki.Application.UseCases.Reports.Commands.UpdateReportStatus
+{
+    public class UpdateReportStatusHandler : IRequestHandler<UpdateReportStatusCommand, OperationResult<bool>>
+    {
+        private readonly IReportRepository _reportRepository;
+
+        public UpdateReportStatusHandler(IReportRepository reportRepository)
+        {
+            _reportRepository = reportRepository;
+        }
+
+        public async Task<OperationResult<bool>> Handle(UpdateReportStatusCommand request, CancellationToken cancellationToken)
+        {
+            var report = await _reportRepository.GetByIdAsync(request.ReportId);
+
+            if (report == null)
+            {
+                return OperationResult<bool>.Failure("Không tìm thấy report.");
+            }
+
+            report.Status = request.NewStatus; 
+            report.AdminReply = request.AdminReply;
+
+           
+            if (request.NewStatus == ReportStatus.Fixed || request.NewStatus == ReportStatus.Rejected)
+            {
+                report.ResolvedAt = DateTime.UtcNow;
+                report.UserHasRead = false; 
+            }
+            else
+            {
+                report.UserHasRead = true;
+            }
+
+            await _reportRepository.UpdateAsync(report);
+
+            return OperationResult<bool>.Success(true);
+        }
+    }
+}
