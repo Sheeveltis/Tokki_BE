f05e637fdbb191428496284ce0f263410515b784
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-09 16:20:14 +0700

    refactor: update vipPackageID for paymentCommand

--- Tokki.Application/UseCases/Payments/Commands/CreatePayment/CreatePaymentCommand.cs
+++ Tokki.Application/UseCases/Payments/Commands/CreatePayment/CreatePaymentCommand.cs
@@ -8,9 +8,9 @@ namespace Tokki.Application.UseCases.Payments.Commands.CreatePayment
 
     public class CreatePaymentCommand : IRequest<OperationResult<CreatePaymentResult>>
     {
+        public string UserId { get; set; } = string.Empty;
         public decimal Amount { get; set; }
         public string Description { get; set; } = string.Empty;
-        public string UserId { get; set; } = string.Empty;
-        public VipPlanType PlanType { get; set; }
+        public string VipPackageId { get; set; }
     }
 }
