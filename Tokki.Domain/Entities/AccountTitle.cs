cea66b8feb72507656c0d56d741ec0f16d2644ba
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-11 00:44:12 +0700

    feat: create title entity

---
+++ Tokki.Domain/Entities/AccountTitle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tokki.Domain.Entities
+{
+    public class AccountTitle
+    {
+        [MaxLength(15)]
+        public string UserId { get; set; } = string.Empty;
+
+        public int TitleId { get; set; }
+
+        public DateTime EarnedAt { get; set; } = DateTime.UtcNow.AddHours(7);
+
+        public virtual Account Account { get; set; }
+        public virtual Title Title { get; set; }
+    }
+}
---
+++ Tokki.Domain/Entities/Title.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tokki.Domain.Entities
+{
+    public class Title
+    {
+        [Key]
+        public int TitleId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty; 
+
+        [MaxLength(255)]
+        public string? Description { get; set; } 
+
+        public long RequiredXP { get; set; } = 0; 
+
+        public bool IsSystemGiven { get; set; } = false; 
+
+        [MaxLength(20)]
+        public string ColorHex { get; set; } = "#000000"; 
+
+        public string? IconUrl { get; set; } 
+    }
+}
