6e654fb7946250caf08fd0e1dd3a73802095a9b1
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-10 16:32:05 +0700

    feat: create GetRevenueChart

---
+++ Tokki.UnitTests/UseCases/Statistics/Queries/GetRevenueChart/GetRevenueChartHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+using Tokki.Application.Common.Models.Statistics;
+using Tokki.Application.IRepositories;
+using Tokki.Application.UseCases.Statistics.Queries;
+
+namespace Tokki.UnitTests.UseCases.Statistics.Queries.GetRevenueChart
+{
+    public class GetRevenueChartHandler : IRequestHandler<GetRevenueChartQuery, OperationResult<RevenueChartDto>>
+    {
+        private readonly IStatisticsRepository _repo;
+        public GetRevenueChartHandler(IStatisticsRepository repo) => _repo = repo;
+
+        public async Task<OperationResult<RevenueChartDto>> Handle(GetRevenueChartQuery request, CancellationToken cancellationToken)
+        {
+            var data = await _repo.GetRevenueChartAsync(request.Year);
+            return OperationResult<RevenueChartDto>.Success(data);
+        }
+    }
+}
---
+++ Tokki.UnitTests/UseCases/Statistics/Queries/GetRevenueChart/GetRevenueChartQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+using Tokki.Application.Common.Models.Statistics;
+
+namespace Tokki.Application.UseCases.Statistics.Queries
+{
+    public class GetRevenueChartQuery : IRequest<OperationResult<RevenueChartDto>>
+    {
+        public int Year { get; set; }
+    }   
+}
