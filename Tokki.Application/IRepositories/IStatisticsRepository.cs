b583f932cd49010a7acc25494b6ec408b52c36c9
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-10 16:26:20 +0700

    feat: create IStatisticsRepository

---
+++ Tokki.Application/IRepositories/IStatisticsRepository.cs
@@ -0,0 +1,20 @@
+﻿using Tokki.Application.Common.Models.Statistics;
+namespace Tokki.Application.IRepositories
+{
+    public interface IStatisticsRepository
+    {
+        Task<DashboardOverviewDto> GetOverviewAsync(DateTime startDate, DateTime endDate);
+
+        Task<RevenueChartDto> GetRevenueChartAsync(int year);
+
+        Task<List<RevenueByPackageDto>> GetRevenueByPackageAsync(DateTime startDate, DateTime endDate);
+
+        Task<(List<TransactionReportDto> Items, int TotalCount)> GetTransactionsAsync(
+            string? search,
+            string? status,
+            DateTime? fromDate,
+            DateTime? toDate,
+            int pageIndex,
+            int pageSize);
+    }
+}
