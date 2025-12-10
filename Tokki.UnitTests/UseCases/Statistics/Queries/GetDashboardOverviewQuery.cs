52c501ff6cb407fbc3ff4f6e56238b9988969aea
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-10 16:30:05 +0700

    feat: create getDashboardOverview

---
+++ Tokki.UnitTests/UseCases/Statistics/Queries/GetDashboardOverviewQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+using Tokki.Application.Common.Models.Statistics;
+
+namespace Tokki.Application.UseCases.Statistics.Queries
+{
+    public class GetDashboardOverviewQuery : IRequest<OperationResult<DashboardOverviewDto>>
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
