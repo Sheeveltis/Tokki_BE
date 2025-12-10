d218fa2a26a02db6c7c3a5888443b6b23c021832
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-11 01:08:02 +0700

    feat: create GamificationController

---
+++ Tokki.WebAPI/Controllers/GamificationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tokki.Application.IServices;
+using Tokki.Application.UseCases.Accounts.DTOs;
+
+namespace Tokki.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // [Authorize]
+    public class GamificationController : ControllerBase
+    {
+        private readonly IGamificationService _gamificationService;
+
+        public GamificationController(IGamificationService gamificationService)
+        {
+            _gamificationService = gamificationService;
+        }
+
+        [HttpPost("heartbeat")]
+        public async Task<IActionResult> TrackStudyTime([FromBody] HeartbeatRequest request)
+        {
+            if (string.IsNullOrEmpty(request.UserId))
+            {
+                return BadRequest("Vui lòng nhập UserId để test.");
+            }
+
+            bool isStreakCompleted = await _gamificationService.TrackStudyTimeAsync(request.UserId, request.DurationInSeconds);
+
+            return Ok(new
+            {
+                Message = "Tracking success",
+                UserId = request.UserId,
+                AddedSeconds = request.DurationInSeconds,
+                IsStreakCompletedJustNow = isStreakCompleted
+            });
+        }
+    }  
+}
