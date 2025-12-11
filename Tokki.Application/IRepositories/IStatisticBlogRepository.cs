75bb820c29e1cbcab4b462bfa53eb3b4113457d8
khoav <khoavnase183164@fpt.edu.vn>
2025-12-11 19:20:41 +0700

    feat: add interface IStatisticBlogRepository

---
+++ Tokki.Application/IRepositories/IStatisticBlogRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokki.Application.UseCases.StatisticBlog.DTOs;
+
+namespace Tokki.Application.IRepositories
+{
+    public interface IStatisticBlogRepository
+    {
+        Task<DashboardStatDTO> GetDashboardStatsAsync(CancellationToken cancellationToken);
+        Task<List<TopBlogDTO>> GetTopViewedBlogsAsync(int count, CancellationToken cancellationToken);
+        Task<List<TopAuthorDTO>> GetTopAuthorsAsync(int count, CancellationToken cancellationToken);
+    }
+}
