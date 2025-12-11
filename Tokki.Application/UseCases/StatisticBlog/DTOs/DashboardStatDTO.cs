624b2316b06076fa14509b66de8312fd014be056
khoav <khoavnase183164@fpt.edu.vn>
2025-12-11 19:20:18 +0700

    feat: add dto for Statistic Blog

---
+++ Tokki.Application/UseCases/StatisticBlog/DTOs/DashboardStatDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokki.Application.UseCases.StatisticBlog.DTOs
+{
+    public class DashboardStatDTO
+    {
+        public int TotalBlogs { get; set; }
+        public long TotalViews { get; set; }
+        public int TotalPublished { get; set; }
+    }
+
+  
+ 
+}
---
+++ Tokki.Application/UseCases/StatisticBlog/DTOs/TopAuthorDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokki.Application.UseCases.StatisticBlog.DTOs
+{
+    public class TopAuthorDTO
+    {
+        public string AuthorId { get; set; } = string.Empty;
+        public int BlogCount { get; set; }
+        public long TotalViews { get; set; }
+    }
+}
