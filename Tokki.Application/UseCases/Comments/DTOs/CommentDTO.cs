1fa61e61983c3a423da035bf157f19d468d1405c
khoav <khoavnase183164@fpt.edu.vn>
2025-12-13 16:08:54 +0700

    feat: update comment

---
+++ Tokki.Application/UseCases/Comments/DTOs/CommentDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Tokki.Application.UseCases.Comments.DTOs
+{
+    public class CommentDTO
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTimeOffset CreatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsEdited { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+        public string AuthorName { get; set; } = string.Empty;
+        public string AuthorAvatar { get; set; } = string.Empty;
+
+        public string? ParentId { get; set; }
+        public List<CommentDTO> Replies { get; set; } = new List<CommentDTO>();
+    }
+}
