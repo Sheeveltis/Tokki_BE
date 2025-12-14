b0d1503a579e48025fb43946ac4fa20e71049698
khoav <khoavnase183164@fpt.edu.vn>
2025-12-15 00:39:36 +0700

    feat: init entities for live chat

---
+++ Tokki.Domain/Entities/ChatRoomMember.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tokki.Domain.Entities
+{
+    public class ChatRoomMember
+    {
+        [Key]
+        [MaxLength(15)]
+        public string ChatRoomMemberId { get; set; } 
+
+        [Required]
+        [MaxLength(10)]
+        public string ChatRoomId { get; set; } = string.Empty;
+
+        [ForeignKey("ChatRoomId")]
+        public virtual ChatRoom ChatRoom { get; set; } = null!;
+
+        [Required]
+        [MaxLength(15)]
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey("UserId")]
+        public virtual Account User { get; set; } = null!;
+
+        public DateTimeOffset JoinedAt { get; set; } = DateTimeOffset.UtcNow;
+
+        public bool IsAdmin { get; set; } = false;
+    }
+}
