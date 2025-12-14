16aa6e98ece5afed9f70ea8e4fef338b2896d1a2
khoav <khoavnase183164@fpt.edu.vn>
2025-12-15 00:40:57 +0700

    refactor: add logic get info user by token for chat hub

--- Tokki.WebAPI/Program.cs
+++ Tokki.WebAPI/Program.cs
@@ -76,6 +76,20 @@ builder.Services.AddAuthentication(options =>
         IssuerSigningKey = new SymmetricSecurityKey(key),
         ClockSkew = TimeSpan.Zero
     };
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Query["access_token"];
+            var path = context.HttpContext.Request.Path;
+            if (!string.IsNullOrEmpty(accessToken) &&
+                (path.StartsWithSegments("/chatHub")))
+            {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
 });
 builder.Services.AddHttpContextAccessor();
 
