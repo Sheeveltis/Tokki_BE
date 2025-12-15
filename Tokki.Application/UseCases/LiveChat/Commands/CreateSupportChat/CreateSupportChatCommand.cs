34be189b1a36a73b3e962e3ef1add74df3ad3317
khoav <khoavnase183164@fpt.edu.vn>
2025-12-15 20:25:01 +0700

    feat: implement create support chat room

---
+++ Tokki.Application/UseCases/LiveChat/Commands/CreateSupportChat/CreateSupportChatCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System.Text.Json.Serialization;
+using Tokki.Application.Common.Models;
+
+namespace Tokki.Application.UseCases.LiveChat.Commands.CreateSupportChat
+{
+    public class CreateSupportChatCommand : IRequest<OperationResult<string>>
+    {
+        [JsonIgnore]
+        public string UserId { get; set; } = string.Empty;
+    }
+}
