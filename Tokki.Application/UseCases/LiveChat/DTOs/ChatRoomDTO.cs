411d2e15c2bd43ba972522bd4970d6d13df9c6dc
khoav <khoavnase183164@fpt.edu.vn>
2025-12-15 20:25:44 +0700

    feat: implement DTO for query

---
+++ Tokki.Application/UseCases/LiveChat/DTOs/ChatRoomDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Tokki.Application.UseCases.LiveChat.DTOs
+{
+    public class ChatRoomDTO
+    {
+        public string ChatRoomId { get; set; } = string.Empty;
+        public string RoomName { get; set; } = string.Empty;
+        public string RoomAvatar { get; set; } = string.Empty;
+        public bool IsSupport { get; set; }
+        public bool IsGroup { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
