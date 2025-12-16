38f88eca98b127fa47a17fd340a3e75d47cd36c2
kietKS <kietnase185061@fpt.edu.vn>
2025-12-16 10:46:01 +0700

    feat flashcard

---
+++ Tokki.Application/UseCases/Vocabulary/Queries/FlashCard/FlashCardQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Tokki.Application.Common.Models;
+using Tokki.Application.UseCases.Vocabulary.DTOs;
+using Tokki.Domain.Enums;
+
+namespace Tokki.Application.UseCases.Vocabulary.Queries.FlashCard
+{
+    public class FlashCardQuery : IRequest<OperationResult<PagedResult<FlashCardDto>>>
+    {
+        public string TopicId { get; set; } = string.Empty;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+    }
+}
