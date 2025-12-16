fa4b5c4938b1bc301e7c83d5b534e63fbe1eca4e
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-12-16 18:55:07 +0700

    feat: create GetAdminExamTemplatesQuery

---
+++ Tokki.Application/UseCases/ExamTemplates/Queries/GetAdminExamTemplates/GetAdminExamTemplatesQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+using Tokki.Application.UseCases.ExamTemplates.DTOs;
+using Tokki.Domain.Enums;
+
+namespace Tokki.Application.UseCases.ExamTemplates.Queries.GetAdminExamTemplates
+{
+    public class GetAdminExamTemplatesQuery : IRequest<OperationResult<PagedResult<AdminExamTemplateDto>>>
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? SearchTerm { get; set; }
+        public ExamTemplateStatus? Status { get; set; }
+    }
+}
