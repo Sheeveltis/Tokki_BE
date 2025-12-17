a71b96214ed95d480f0120c6401fd08216ed2d07
khoav <khoavnase183164@fpt.edu.vn>
2025-12-17 19:22:12 +0700

    feat: add submit review vocab

---
+++ Tokki.Application/UseCases/VocabSpacedRepetition/DTOs/ReviewResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokki.Application.UseCases.VocabSpacedRepetition.DTOs
+{
+    public class ReviewResponse
+    {
+        public string VocabularyId { get; set; }
+
+        public bool IsMastered { get; set; }
+    }
+}
