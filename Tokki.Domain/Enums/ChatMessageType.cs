084605ed8aaee777c54981551c3c284a20fa5be3
khoav <khoavnase183164@fpt.edu.vn>
2025-12-25 22:36:34 +0700

    feat: add enum type chat message

---
+++ Tokki.Domain/Enums/ChatMessageType.cs
@@ -0,0 +1,16 @@
+﻿namespace Tokki.Domain.Enums
+{
+    public enum ChatMessageType
+    {
+        //Tin nhắn thường 
+        Text = 0,       
+        //Ảnh
+        Image = 1,      
+        //File
+        File = 2,     
+        //Audio
+        Audio = 3,    
+        //TIn nhắn hệ thống ví dụ kiểu Fuwy đã tham gia phòng chat
+        System = 9     
+    }
+}
