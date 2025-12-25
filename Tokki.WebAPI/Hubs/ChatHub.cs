75038b38034bcef76644e5c41cf1fe2d4fdc02f7
khoav <khoavnase183164@fpt.edu.vn>
2025-12-25 22:37:46 +0700

    refactor: changing LiveChatMessage to ChatMessage

--- Tokki.WebAPI/Hubs/ChatHub.cs
+++ Tokki.WebAPI/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
+using Tokki.Application.IRepositories;
 using Tokki.Application.IServices;
 using Tokki.Domain.Entities;
 using Tokki.Infrastructure.Data;
@@ -10,60 +11,49 @@ namespace Tokki.WebAPI.Hubs
     [Authorize]
     public class ChatHub : Hub
     {
-        private readonly IChatService _mongoChatService; 
-        private readonly TokkiDbContext _sqlContext;    
+        private readonly IChatRepository _mongoChatService;
+        private readonly IAccountRepository _accountRepo;
         private readonly IIdGeneratorService _idGen;
 
         public ChatHub(
-            IChatService mongoChatService,
-            TokkiDbContext sqlContext,
+            IChatRepository mongoChatService,
+            IAccountRepository accountRepo,
             IIdGeneratorService idGen)
         {
             _mongoChatService = mongoChatService;
-            _sqlContext = sqlContext;
+            _accountRepo = accountRepo;
             _idGen = idGen;
         }
-
         /// <summary>
         /// Hàm này Client gọi để gửi tin nhắn
         /// </summary>
         public async Task SendMessage(string roomId, string content)
         {
             var userId = Context.UserIdentifier;
-
             if (string.IsNullOrEmpty(userId))
             {
                 throw new HubException("Không xác định được người dùng.");
             }
-            var userInfo = await _sqlContext.Accounts
-                                .AsNoTracking()
-                                .Where(u => u.UserId == userId)
-                                .Select(u => new
-                                {
-                                    u.FullName,
-                                    u.AvatarUrl
-                                })
-                                .FirstOrDefaultAsync();
+
+            var userInfo = await _accountRepo.GetBasicInfoAsync(userId);
 
             if (userInfo == null)
             {
                 throw new HubException("Tài khoản không tồn tại trong hệ thống.");
             }
-
-            var chatMsg = new LiveChatMessage
+            var chatMsg = new ChatMessage
             {
-                LiveChatMessageId = _idGen.Generate(),
+                ChatMessageId = _idGen.Generate(21),
                 Content = content,
                 RoomId = roomId,
                 CreatedAt = DateTime.UtcNow,
                 SenderId = userId,
-                SenderName = userInfo.FullName ?? "Unknown User",
-                SenderAvatar = userInfo.AvatarUrl ?? ""
+                SenderName = userInfo.FullName,
+                SenderAvatar = userInfo.AvatarUrl ?? "", 
             };
             await _mongoChatService.CreateMessageAsync(chatMsg);
             await Clients.Group(roomId).SendAsync("ReceiveMessage", chatMsg);
         }
-
         /// <summary>
         /// Hàm này Client gọi khi user bấm vào một phòng chat/lớp học
         /// </summary>
