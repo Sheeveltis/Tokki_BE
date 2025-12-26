3472c0c9e46e85afbd4f3aefab0c8ccbcb953619
khoav <khoavnase183164@fpt.edu.vn>
2025-12-26 23:54:23 +0700

    feat: DTO for vocab excel

---
+++ Tokki.Application/UseCases/Excel/Commands/DTOs/VocabularyExcelDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokki.Application.UseCases.Excel.Commands.DTOs
+{
+    public class VocabularyExcelDTO
+    {
+        public string Text { get; set; }
+        public string Pronunciation { get; set; }
+        public string ImageUrl { get; set; } 
+        public string Definition { get; set; }
+    }
+    public class ImportVocabularyResponse
+    {
+        public List<VocabularyPreviewDTO> SuccessList { get; set; } = new();
+        public List<VocabularyPreviewDTO> FailureList { get; set; } = new();
+    }
+
+    public class VocabularyPreviewDTO
+    {
+        public string Text { get; set; }
+        public string Definition { get; set; }
+        public string Pronunciation { get; set; }
+        public string? ImageUrl { get; set; }
+        public string Reason { get; set; }
+    }
+}
