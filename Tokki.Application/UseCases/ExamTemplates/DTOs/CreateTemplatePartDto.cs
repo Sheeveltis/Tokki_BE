be780bffd7b9d0c853950d22aa239fbf030b19dd
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-01-06 01:00:43 +0700

    refactor: update mark and ExampleUrl

--- Tokki.Application/UseCases/ExamTemplates/DTOs/CreateTemplatePartDto.cs
+++ Tokki.Application/UseCases/ExamTemplates/DTOs/CreateTemplatePartDto.cs
@@ -4,16 +4,13 @@ namespace Tokki.Application.UseCases.ExamTemplates.DTOs
 {
     public class CreateTemplatePartDto
     {
-        public QuestionSkill Skill { get; set; } 
+        public QuestionSkill Skill { get; set; }
         public int QuestionFrom { get; set; }
         public int QuestionTo { get; set; }
         public string? PartTitle { get; set; }
         public string? Instruction { get; set; }
-
-        public DifficultyLevel DifficultyLevel { get; set; } 
+        public int Mark { get; set; } 
         public string QuestionTypeId { get; set; } = string.Empty;
-
-        public ExampleType ExampleType { get; set; } = ExampleType.None;
-        public string? ExampleData { get; set; }
+        public string? ExampleUrl { get; set; } 
     }
 }
