25c5b69105392488a9d48524ce53c82aeeb27bae
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-01-06 02:05:59 +0700

    refactor: add TemplatePartStatus

--- Tokki.Domain/Entities/TemplatePart.cs
+++ Tokki.Domain/Entities/TemplatePart.cs
@@ -34,7 +34,8 @@ namespace Tokki.Domain.Entities
         [MaxLength(10)]
         public string QuestionTypeId { get; set; } = string.Empty;
 
-        public string? ExampleUrl { get; set; } 
+        public string? ExampleUrl { get; set; }
+        public TemplatePartStatus Status { get; set; } = TemplatePartStatus.Active;
 
         [ForeignKey(nameof(ExamTemplateId))]
         public virtual ExamTemplate ExamTemplate { get; set; } = null!;
