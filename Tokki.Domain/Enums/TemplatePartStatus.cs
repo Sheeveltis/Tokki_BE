0bc0d313d5ea46cc6cc66ff92a621378249f99bb
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-01-06 02:05:08 +0700

    feat: create TemplatePartStatus

---
+++ Tokki.Domain/Enums/TemplatePartStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Tokki.Domain.Enums
+{
+    public enum TemplatePartStatus
+    {
+        [Description("Đang hoạt động")]
+        Active = 1,
+
+        [Description("Đã xóa")]
+        Deleted = 0
+    }
+}
