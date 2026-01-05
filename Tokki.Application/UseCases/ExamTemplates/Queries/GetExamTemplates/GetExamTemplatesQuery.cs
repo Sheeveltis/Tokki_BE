1c0b6b60ec583659f856a2d961c0432a68d54aab
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-01-06 01:14:52 +0700

    feat: create UserGetViewExamTemplate

--- Tokki.Application/UseCases/ExamTemplates/Queries/GetExamTemplates/GetExamTemplatesQuery.cs
+++ Tokki.Application/UseCases/ExamTemplates/Queries/GetExamTemplates/GetExamTemplatesQuery.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MediatR;
+﻿using MediatR;
 using Tokki.Application.Common.Models;
 using Tokki.Application.UseCases.ExamTemplates.DTOs;
 using Tokki.Domain.Enums;
 
 namespace Tokki.Application.UseCases.ExamTemplates.Queries.GetExamTemplates
 {
-    public class GetExamTemplatesQuery : IRequest<OperationResult<PagedResult<ExamTemplateDto>>>
+    public class GetExamTemplatesQuery : IRequest<OperationResult<(IEnumerable<ExamTemplateDto> Items, int TotalCount)>>
     {
-        public string? SearchTerm { get; set; }
-        public ExamTemplateStatus? Status { get; set; }
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 10;
+        public string? SearchTerm { get; set; }
+        public ExamType? Type { get; set; } 
     }
 }
