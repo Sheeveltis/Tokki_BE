4458d2f5a4fcc36d10559714f46b353e4246f83a
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-01-06 01:51:42 +0700

    feat: create UpdateTemplatePartCommandValidator

---
+++ Tokki.Application/UseCases/TemplateParts/Commands/UpdateTemplatePart/UpdateTemplatePartCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Tokki.Application.UseCases.TemplateParts.Commands.UpdateTemplatePart;
+
+namespace Tokki.Application.UseCases.TemplateParts.Commands.UpdateTemplatePart
+{
+    public class UpdateTemplatePartCommandValidator : AbstractValidator<UpdateTemplatePartCommand>
+    {
+        public UpdateTemplatePartCommandValidator()
+        {
+            RuleFor(x => x.TemplatePartId).NotEmpty();
+
+            RuleFor(x => x.Skill).IsInEnum();
+
+            RuleFor(x => x.QuestionFrom).GreaterThan(0);
+            RuleFor(x => x.QuestionTo).GreaterThan(0);
+            RuleFor(x => x.QuestionTo).GreaterThanOrEqualTo(x => x.QuestionFrom);
+
+            RuleFor(x => x.PartTitle).NotEmpty().MaximumLength(150);
+
+            RuleFor(x => x.Mark).GreaterThan(0).WithName("Điểm số");
+
+            RuleFor(x => x.QuestionTypeId).NotEmpty();
+        }
+    }
+}
