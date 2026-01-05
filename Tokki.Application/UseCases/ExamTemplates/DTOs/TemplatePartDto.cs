d61051b1e91d989300f728930395e28b506b94ab
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-01-06 01:00:05 +0700

    rafactor: update templatePartDTO

--- Tokki.Application/UseCases/ExamTemplates/DTOs/TemplatePartDto.cs
+++ Tokki.Application/UseCases/ExamTemplates/DTOs/TemplatePartDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tokki.Domain.Enums;
+﻿using Tokki.Domain.Enums;
 
 namespace Tokki.Application.UseCases.ExamTemplates.DTOs
 {
@@ -15,7 +10,9 @@ namespace Tokki.Application.UseCases.ExamTemplates.DTOs
         public int QuestionTo { get; set; }
         public string? PartTitle { get; set; }
         public string? Instruction { get; set; }
-        public ExampleType ExampleType { get; set; }
-        public string? ExampleData { get; set; }
+        public int Mark { get; set; }
+        public string? ExampleUrl { get; set; }
+        public string QuestionTypeId { get; set; } = string.Empty;
+        public string QuestionTypeName { get; set; } = string.Empty; 
     }
 }
