1abd2abcb786a90e7d172b775923c6fbac2ef6b6
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-01-06 01:03:14 +0700

    feat: add validation for createExamTemplate

--- Tokki.Application/UseCases/ExamTemplates/Commands/CreateExamTemplate/CreateExamTemplateCommandValidator.cs
+++ Tokki.Application/UseCases/ExamTemplates/Commands/CreateExamTemplate/CreateExamTemplateCommandValidator.cs
@@ -1,70 +1,27 @@
 ﻿using FluentValidation;
-using Tokki.Application.UseCases.ExamTemplates.Commands.CreateExamTemplate;
 
-public class CreateExamTemplateCommandValidator : AbstractValidator<CreateExamTemplateCommand>
+namespace Tokki.Application.UseCases.ExamTemplates.Commands.CreateExamTemplate
 {
-    public CreateExamTemplateCommandValidator()
+    public class CreateExamTemplateCommandValidator : AbstractValidator<CreateExamTemplateCommand>
     {
-        // 1. Name
-        // Sử dụng: NotEmptyValidator, MaximumLengthValidator
-        RuleFor(x => x.Name)
-            .NotEmpty()
-            .MaximumLength(100)
-            .WithName("Tên mẫu đề thi");
-
-        // 2. Description
-        // Sử dụng: MaximumLengthValidator
-        RuleFor(x => x.Description)
-            .MaximumLength(255)
-            .When(x => !string.IsNullOrEmpty(x.Description))
-            .WithName("Mô tả");
-
-        // 3. Parts (Danh sách)
-        // Sử dụng: NotEmptyValidator (Thông báo sẽ là: 'Danh sách phần thi' không được để trống.)
-        RuleFor(x => x.Parts)
-            .NotEmpty()
-            .WithName("Danh sách phần thi");
-
-        // 4. Các rule con trong Parts
-        RuleForEach(x => x.Parts).ChildRules(part =>
+        public CreateExamTemplateCommandValidator()
         {
-            // Skill
-            // Sử dụng: EnumValidator
-            part.RuleFor(p => p.Skill)
-                .IsInEnum()
-                .WithName("Kỹ năng");
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(100).WithName("Tên mẫu đề thi");
+            RuleFor(x => x.Description).MaximumLength(255).WithName("Mô tả");
+            RuleFor(x => x.Type).IsInEnum().WithName("Loại đề");
 
-            // QuestionFrom
-            // Sử dụng: GreaterThanValidator
-            part.RuleFor(p => p.QuestionFrom)
-                .GreaterThan(0)
-                .WithName("Câu bắt đầu");
-
-            // QuestionTo
-            // Sử dụng: GreaterThanValidator
-            part.RuleFor(p => p.QuestionTo)
-                .GreaterThan(0)
-                .WithName("Câu kết thúc");
-
-            // Logic QuestionTo >= QuestionFrom
-            // Sử dụng: GreaterThanOrEqualValidator
-            // Thông báo sẽ là: 'Câu kết thúc' phải lớn hơn hoặc bằng '{Giá trị của QuestionFrom}'.
-            part.RuleFor(p => p.QuestionTo)
-                .GreaterThanOrEqualTo(p => p.QuestionFrom)
-                .WithName("Câu kết thúc");
-
-            // PartTitle
-            // Sử dụng: MaximumLengthValidator
-            part.RuleFor(p => p.PartTitle)
-                .MaximumLength(255)
-                .When(p => !string.IsNullOrEmpty(p.PartTitle))
-                .WithName("Tiêu đề phần");
-
-            // ExampleType
-            // Sử dụng: EnumValidator
-            part.RuleFor(p => p.ExampleType)
-                .IsInEnum()
-                .WithName("Loại ví dụ");
-        });
+            // Validate Parts
+            RuleFor(x => x.Parts).NotEmpty().WithName("Danh sách phần thi");
+            RuleForEach(x => x.Parts).ChildRules(part =>
+            {
+                part.RuleFor(p => p.Skill).IsInEnum().WithName("Kỹ năng");
+                part.RuleFor(p => p.QuestionFrom).GreaterThan(0).WithName("Câu bắt đầu");
+                part.RuleFor(p => p.QuestionTo).GreaterThan(0).WithName("Câu kết thúc");
+                part.RuleFor(p => p.QuestionTo).GreaterThanOrEqualTo(p => p.QuestionFrom).WithName("Câu kết thúc");
+                part.RuleFor(p => p.PartTitle).MaximumLength(255).WithName("Tiêu đề phần");
+                part.RuleFor(p => p.Mark).GreaterThan(0).WithName("Điểm số");
+                part.RuleFor(p => p.QuestionTypeId).NotEmpty().WithName("Loại câu hỏi");
+            });
+        }
     }
 }
