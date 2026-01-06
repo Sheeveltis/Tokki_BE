f142e17886ecbe487367dc90f8ff3e7cb4bde1a8
kietKS <kietnase185061@fpt.edu.vn>
2026-01-07 01:57:00 +0700

    feat api change text to voice url

---
+++ Tokki.WebAPI/Controllers/TextToSpeechController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Tokki.Application.UseCases.TextToSpeech.Commands.GenerateVocabularyAudioUrl;
+
+namespace Tokki.WebAPI.Controllers
+{
+    [Route("api/tts")]
+    [ApiController]
+    public class TextToSpeechController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public TextToSpeechController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("vocabulary-audio-url")]
+        public async Task<IActionResult> GenerateVocabularyAudioUrl([FromBody] GenerateVocabularyAudioUrlCommand command)
+        {
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
