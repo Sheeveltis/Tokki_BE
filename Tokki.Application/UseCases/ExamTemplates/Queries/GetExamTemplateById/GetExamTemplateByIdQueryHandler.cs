646867b32eba615b47ff29ebd555accb0a9baf40
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-01-08 00:29:16 +0700

    refactor: change time to VN locate

--- Tokki.Application/UseCases/ExamTemplates/Queries/GetExamTemplateById/GetExamTemplateByIdQueryHandler.cs
+++ Tokki.Application/UseCases/ExamTemplates/Queries/GetExamTemplateById/GetExamTemplateByIdQueryHandler.cs
@@ -24,7 +24,7 @@ public class GetExamTemplateByIdQueryHandler : IRequestHandler<GetExamTemplateBy
             ExamTemplateId = et.ExamTemplateId,
             Name = et.Name,
             Description = et.Description,
-            CreatedAt = et.CreatedAt,
+            CreatedAt = et.CreatedAt.AddHours(7),
             Status = et.Status,
             Type = et.Type,
             TotalParts = et.TemplateParts.Count,
