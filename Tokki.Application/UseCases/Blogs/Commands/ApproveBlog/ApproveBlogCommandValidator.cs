5c3ba90841a12f26fbcab3caf9ed3b5976934c4d
khoav <khoavnase183164@fpt.edu.vn>
2026-01-12 09:21:36 +0700

    feat: implement api for approve blog

---
+++ Tokki.Application/UseCases/Blogs/Commands/ApproveBlog/ApproveBlogCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokki.Application.UseCases.Blogs.Commands.ApproveBlog
+{
+    public class ApproveBlogCommandValidator : AbstractValidator<ApproveBlogCommand>
+    {
+        public ApproveBlogCommandValidator()
+        {
+            RuleFor(x => x.BlogId)
+                .NotEmpty()
+                .WithName("BlogId");
+
+
+        }
+    }
+}
