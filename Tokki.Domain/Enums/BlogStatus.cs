18d8a41a280da8577b92e7839a02f5c5e27ea0b6
khoav <khoavnase183164@fpt.edu.vn>
2026-01-12 09:22:08 +0700

    feat: add status reject + pending

--- Tokki.Domain/Enums/BlogStatus.cs
+++ Tokki.Domain/Enums/BlogStatus.cs
@@ -13,11 +13,14 @@ namespace Tokki.Domain.Enums
 
         [Description("Đã ẩn")]
         Hidden = 2,
-
         //Lưu trữ (Kiểu Published thì sẽ ưu tiên hiện cho mn xem, còn Archived này thì nó không ưu tiên hiện, ai có link mới truy cập 
         //Ví dụ Lịch đăng ký thi TOPIK 2022 thì ko cần hiển thị lên cho mn, nhưng cũng ko nhất thiết phải xóa
         [Description("Lưu trữ")]
-        Archived = 3
+        Archived = 3,
+        [Description("Chờ phê duyệt")]
+        PendingApproval = 4,
+        [Description("Đã từ chối")]
+        Rejected = 5
 
     }
 }
