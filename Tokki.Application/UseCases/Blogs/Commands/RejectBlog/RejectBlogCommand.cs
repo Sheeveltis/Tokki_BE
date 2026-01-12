c0da874a77abd52e5fb0af9d812277d203e5d7b4
khoav <khoavnase183164@fpt.edu.vn>
2026-01-12 09:21:55 +0700

    feat: implement for reject blog

---
+++ Tokki.Application/UseCases/Blogs/Commands/RejectBlog/RejectBlogCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokki.Application.Common.Models;
+
+namespace Tokki.Application.UseCases.Blogs.Commands.RejectBlog
+{
+    public class RejectBlogCommand : IRequest<OperationResult<bool>>
+    {
+        public string BlogId { get; set; } = string.Empty;
+        public string RejectReason { get; set; } = string.Empty;
+    }
+}
---
+++ Tokki.Application/UseCases/Blogs/Commands/RejectBlog/RejectBlogCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokki.Application.UseCases.Blogs.Commands.RejectBlog
+{
+    public class RejectBlogCommandValidator : AbstractValidator<RejectBlogCommand>
+    {
+        public RejectBlogCommandValidator()
+        {
+            RuleFor(x => x.BlogId)
+                .NotEmpty()
+                .WithName("BlogId");
+
+            RuleFor(x => x.RejectReason)
+                .NotEmpty().WithMessage(" là bắt buộc.")
+                .MinimumLength(10)
+                .MaximumLength(2000)
+                .WithName("Lý do từ chối");
+        }
+    }
+}
