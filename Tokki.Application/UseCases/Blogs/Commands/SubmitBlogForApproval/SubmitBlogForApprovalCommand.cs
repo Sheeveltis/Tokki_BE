42a2810ab5ed18a4a54912a7454b01f5a7c5298e
khoav <khoavnase183164@fpt.edu.vn>
2026-01-12 09:22:23 +0700

    feat: implement submit blog for approval

---
+++ Tokki.Application/UseCases/Blogs/Commands/SubmitBlogForApproval/SubmitBlogForApprovalCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokki.Application.Common.Models;
+
+namespace Tokki.Application.UseCases.Blogs.Commands.SubmitBlogForApproval
+{
+    public class SubmitBlogForApprovalCommand : IRequest<OperationResult<bool>>
+    {
+        public string BlogId { get; set; } = string.Empty;
+    }
+}
