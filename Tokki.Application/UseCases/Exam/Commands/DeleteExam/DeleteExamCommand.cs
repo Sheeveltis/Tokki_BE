82599070020c22f2b4438ce0d8b5215a74d31023
khoav <khoavnase183164@fpt.edu.vn>
2026-01-28 22:36:51 +0700

    feat: implement api delete exam

---
+++ Tokki.Application/UseCases/Exam/Commands/DeleteExam/DeleteExamCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Tokki.Application.Common.Models;
+
+namespace Tokki.Application.UseCases.Exam.Commands.DeleteExam
+{
+    public class DeleteExamCommand : IRequest<OperationResult<string>>
+    {
+        [JsonIgnore]
+        public string ExamId { get; set; } = string.Empty;
+    }
+}
