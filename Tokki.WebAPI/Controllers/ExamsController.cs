e1cd297e25977afb03b7b1171fe7c6d1e85359a9
khoav <khoavnase183164@fpt.edu.vn>
2026-01-28 22:37:38 +0700

    feat: implement new api read, update, delete exam

--- Tokki.WebAPI/Controllers/ExamsController.cs
+++ Tokki.WebAPI/Controllers/ExamsController.cs
@@ -2,13 +2,18 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using Tokki.Application.Common.Models;
 using Tokki.Application.UseCases.Exam.Commands.AddQuestionToExam;
 using Tokki.Application.UseCases.Exam.Commands.CreateExam;
+using Tokki.Application.UseCases.Exam.Commands.DeleteExam;
 using Tokki.Application.UseCases.Exam.Commands.RegenerateExamPart;
 using Tokki.Application.UseCases.Exam.Commands.RemoveQuestionFromExam;
+using Tokki.Application.UseCases.Exam.Commands.UpdateExamInfo;
+using Tokki.Application.UseCases.Exam.Commands.UpdateExamStatus;
 using Tokki.Application.UseCases.Exam.Queries.GetExamById;
 using Tokki.Application.UseCases.Exam.Queries.GetExamDetailQuery;
 using Tokki.Application.UseCases.Exam.Queries.GetExams;
+using Tokki.Application.UseCases.Exam.Queries.GetQuestionsByPart;
 
 namespace Tokki.WebAPI.Controllers
 {
@@ -60,9 +65,9 @@ namespace Tokki.WebAPI.Controllers
         }
         [HttpGet("admin/detail")]
         [Authorize(Roles = "Admin,Staff")]
-        public async Task<IActionResult> GetExamDetail(string id)
+        public async Task<IActionResult> GetExamDetail(string examId)
         {
-            var query = new GetExamDetailQuery { ExamId = id };
+            var query = new GetExamDetailQuery { ExamId = examId };
 
             var result = await _sender.Send(query);
 
@@ -72,6 +77,77 @@ namespace Tokki.WebAPI.Controllers
             }
 
             return StatusCode(result.StatusCode, result);
+        }
+        [HttpGet("get-questions-by-part")] 
+        [Authorize(Roles = "Admin,Staff")]
+        public async Task<IActionResult> GetAvailableQuestions(
+            string templatePartId,
+            [FromQuery] int pageNumber = 1,
+            [FromQuery] int pageSize = 10,
+            [FromQuery] string? search = null)
+        {
+            var query = new GetQuestionsByPartQuery
+            {
+                TemplatePartId = templatePartId,
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                SearchTerm = search
+            };
+
+            var result = await _sender.Send(query);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+
+            if (result.StatusCode == 404)
+            {
+                return NotFound(result);
+            }
+
+            return BadRequest(result);
+        }
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin,Staff")]
+        public async Task<IActionResult> UpdateExamInfo(string id, [FromBody] UpdateExamInfoCommand command)
+        {
+            command.ExamId = id;
+            var result = await _sender.Send(command);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPut("{id}/status")]
+        [Authorize(Roles = "Admin,Staff")]
+        public async Task<IActionResult> UpdateStatus(string id, [FromBody] UpdateExamStatusCommand command)
+        {
+            command.ExamId = id;
+            var result = await _sender.Send(command);
+            if (result.IsSuccess) return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpPut("update-exam-question")]
+        [Authorize(Roles = "Admin,Staff")]
+        public async Task<IActionResult> UpdateExamQuestions([FromBody] AddQuestionToExamCommand command)
+        {
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+       
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin,Staff")]
+        public async Task<IActionResult> DeleteExam(string id)
+        {
+            var command = new DeleteExamCommand { ExamId = id };
+            var result = await _sender.Send(command);
+
+            if (result.IsSuccess) return Ok(result);
+            if (result.StatusCode == 404) return NotFound(result);
+            return BadRequest(result);
         }
     }
 }
