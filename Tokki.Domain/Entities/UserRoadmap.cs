94c32584119df0211d8d9e30aaa3eb4f397de8aa
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-02-07 00:35:00 +0700

    feat: create UserRoadmap entities

---
+++ Tokki.Domain/Entities/UserRoadmap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Tokki.Domain.Enums;
+
+namespace Tokki.Domain.Entities
+{
+    public class UserRoadmap
+    {
+        [Key]
+        [MaxLength(15)]
+        public string UserRoadmapId { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(15)]
+        public string UserId { get; set; } = string.Empty;
+
+        [MaxLength(50)]
+        public string TargetAim { get; set; } = string.Empty; 
+
+        public int DurationDays { get; set; } 
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public UserRoadmapStatus CurrentStatus { get; set; } = UserRoadmapStatus.Active;
+
+        public string? OverallAiAssessment { get; set; }
+
+        public string? SystemPromptContext { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey(nameof(UserId))]
+        public virtual Account Account { get; set; } = null!;
+
+        public virtual ICollection<RoadmapWeek> Weeks { get; set; } = new List<RoadmapWeek>();
+        public virtual ICollection<RoadmapKnowledgeProfile> KnowledgeProfiles { get; set; } = new List<RoadmapKnowledgeProfile>();
+    }
+}
