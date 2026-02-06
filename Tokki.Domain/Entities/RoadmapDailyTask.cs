9fd1e3a4c3ffd466400d95451f9d5fd56ea02e79
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-02-07 00:36:47 +0700

    feat: create RoadmapDailyTask

---
+++ Tokki.Domain/Entities/RoadmapDailyTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Tokki.Domain.Enums;
+
+namespace Tokki.Domain.Entities
+{
+    public class RoadmapDailyTask
+    {
+        [Key]
+        [MaxLength(15)]
+        public string TaskId { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(15)]
+        public string RoadmapWeekId { get; set; } = string.Empty;
+
+        public int DayIndex { get; set; } 
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        public RoadmapTaskType TaskType { get; set; }
+
+        public string? AiGeneratedContent { get; set; }
+
+        [MaxLength(10)]
+        public string? TargetQuestionTypeId { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+        public DateTime? CompletionDate { get; set; }
+
+        [ForeignKey(nameof(RoadmapWeekId))]
+        public virtual RoadmapWeek RoadmapWeek { get; set; } = null!;
+
+        [ForeignKey(nameof(TargetQuestionTypeId))]
+        public virtual QuestionType? TargetQuestionType { get; set; }
+    }
+}
