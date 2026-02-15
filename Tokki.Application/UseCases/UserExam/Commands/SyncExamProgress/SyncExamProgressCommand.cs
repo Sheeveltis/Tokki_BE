a0d929b018b7dc86ef5afc06f5539110f324c442
khoav <khoavnase183164@fpt.edu.vn>
2026-02-15 12:57:48 +0700

    feat: implement system sync exam progress

---
+++ Tokki.Application/UseCases/UserExam/Commands/SyncExamProgress/SyncExamProgressCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Tokki.Application.Common.Models;
+using Tokki.Application.UseCases.UserExam.DTOs;
+
+namespace Tokki.Application.UseCases.UserExam.Commands.SyncExamProgress
+{
+    public class SyncExamProgressCommand : IRequest<OperationResult<bool>>
+    {
+        [JsonIgnore]
+        public string UserId { get; set; } = string.Empty;
+        public string UserExamId { get; set; } = string.Empty;
+        public List<AnswerSyncDto> Answers { get; set; } = new();
+    }
+}
---
+++ Tokki.Application/UseCases/UserExam/Commands/SyncExamProgress/SyncExamProgressCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokki.Application.Common.Models;
+using Tokki.Application.IRepositories;
+
+namespace Tokki.Application.UseCases.UserExam.Commands.SyncExamProgress
+{
+    public class SyncExamProgressCommandHandler : IRequestHandler<SyncExamProgressCommand, OperationResult<bool>>
+    {
+        private readonly IUserExamRepository _repository;
+
+        public SyncExamProgressCommandHandler(IUserExamRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<OperationResult<bool>> Handle(SyncExamProgressCommand request, CancellationToken token)
+        {
+            var session = await _repository.GetByIdAsync(request.UserExamId, token);
+            if (session == null) return OperationResult<bool>.Failure("Không tìm thấy phiên làm bài", 404);
+
+            var mcqMap = session.UserExamAnswers.ToDictionary(x => x.UserExamAnswerId);
+            var writingMap = session.UserExamWritingAnswers.ToDictionary(x => x.UserExamWritingAnswerId);
+
+            foreach (var incoming in request.Answers)
+            {
+                if (mcqMap.TryGetValue(incoming.UserAnswerId, out var mcq))
+                {
+                    if (mcq.SelectedOptionId != incoming.SelectedOptionId)
+                    {
+                        mcq.SelectedOptionId = incoming.SelectedOptionId;
+
+                        var correctId = mcq.Question.QuestionOptions.FirstOrDefault(o => o.IsCorrect)?.OptionId;
+                        mcq.IsCorrect = (mcq.SelectedOptionId == correctId);
+                    }
+                }
+                else if (writingMap.TryGetValue(incoming.UserAnswerId, out var writing))
+                {
+                    if (writing.AnswerContent != incoming.AnswerContent)
+                    {
+                        writing.AnswerContent = incoming.AnswerContent ?? string.Empty;
+                        writing.WordCount = writing.AnswerContent.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+                    }
+                }
+            }
+
+            await _repository.SaveChangesAsync(token);
+            return OperationResult<bool>.Success(true);
+        }
+
+    }
+}
