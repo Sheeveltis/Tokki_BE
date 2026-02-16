25ee98d5896772bacbf4026b5d36d29b48653bf0
khoav <khoavnase183164@fpt.edu.vn>
2026-02-16 14:55:17 +0700

    feat: init entities & enum for wordle

---
+++ Tokki.Domain/Entities/DailyWordle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokki.Domain.Enums;
+
+namespace Tokki.Domain.Entities
+{
+    public class DailyWordle
+    {
+        [Key]
+        [MaxLength(20)] 
+        public string DailyWordleId { get; set; } = string.Empty;
+
+        public DateOnly GameDate { get; set; }
+
+        public WordleLevel Level { get; set; }
+
+        [MaxLength(10)]
+        public string Word { get; set; } = string.Empty;
+
+        [MaxLength(15)]
+        public string VocabularyId { get; set; } = string.Empty;
+
+        [ForeignKey("VocabularyId")]
+        public virtual Vocabulary? Vocabulary { get; set; }
+    }
+}
---
+++ Tokki.Domain/Entities/UserWordleProgress.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+
+namespace Tokki.Domain.Entities
+{
+    public class UserWordleProgress
+    {
+        [Key]
+        [MaxLength(20)]
+        public string UserWordleProgressId { get; set; } = string.Empty;
+
+        [MaxLength(15)]
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey("UserId")]
+        public virtual Account User { get; set; } = null!; 
+
+        [MaxLength(20)]
+        public string DailyWordleId { get; set; } = string.Empty;
+
+        [ForeignKey("DailyWordleId")]
+        public virtual DailyWordle DailyWordle { get; set; } = null!;
+
+        public string GuessesJson { get; set; } = "[]";
+
+        [NotMapped]
+        public List<string> Guesses
+        {
+            get => string.IsNullOrEmpty(GuessesJson)
+                   ? new List<string>()
+                   : JsonSerializer.Deserialize<List<string>>(GuessesJson) ?? new List<string>();
+            set => GuessesJson = JsonSerializer.Serialize(value);
+        }
+
+        public int AttemptCount { get; set; }
+        public bool IsWon { get; set; }
+        public DateTime LastActivity { get; set; } = DateTime.UtcNow;
+    }
+}
