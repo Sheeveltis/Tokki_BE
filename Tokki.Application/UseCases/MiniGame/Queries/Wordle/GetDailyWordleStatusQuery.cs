1672d01685bc1b2ec4a2fccbd6bdda7e3c89745f
khoav <khoavnase183164@fpt.edu.vn>
2026-02-16 14:56:45 +0700

    feat: implement get dashboard wordle

---
+++ Tokki.Application/UseCases/MiniGame/Queries/Wordle/GetDailyWordleStatusQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Tokki.Application.Common.Models;
+using Tokki.Application.UseCases.MiniGame.DTOs;
+
+namespace Tokki.Application.UseCases.MiniGame.Queries.Wordle
+{
+    public class GetDailyWordleStatusQuery : IRequest<OperationResult<WordleDashboardDTO>>
+    {
+        [JsonIgnore]
+        public string UserId { get; set; }
+    }
+}
