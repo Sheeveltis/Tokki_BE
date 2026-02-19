1e2c91d640d939c968c61665a50ff667530aa07c
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-02-20 00:08:51 +0700

    feat: add new enum for each topik level

---
+++ Tokki.Domain/Enums/CurrentTopikLevel.cs
@@ -0,0 +1,14 @@
+﻿namespace Tokki.Domain.Enums
+{
+    public enum CurrentTopikLevel
+    {
+        Pre_Topik = 0, 
+        Level_1 = 1,   
+        Level_2 = 2,   
+
+        Level_3 = 3,   
+        Level_4 = 4,   
+        Level_5 = 5,   
+        Level_6 = 6    
+    }
+}
---
+++ Tokki.Domain/Enums/TargetAimLevel.cs
@@ -0,0 +1,8 @@
+﻿namespace Tokki.Domain.Enums
+{
+    public enum TargetAimLevel
+    {
+        Topik_I = 1,
+        Topik_II = 2 
+    }
+}
