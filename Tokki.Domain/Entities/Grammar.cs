cf782bc2fd3e47cf107d60915fe3e012c8d6ed60
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-02-20 00:11:02 +0700

    feat: add entity for grammar and KnowledgeMetadata

---
+++ Tokki.Domain/Entities/Grammar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Tokki.Domain.Enums;
+
+namespace Tokki.Domain.Entities
+{
+    public class Grammar
+    {
+        [Key]
+        public string GrammarId { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Syntaxes { get; set; } 
+
+        public string? Description { get; set; } 
+
+        public CurrentTopikLevel Level { get; set; } 
+
+        public string? RelatedQuestionTypeId { get; set; }
+
+        [ForeignKey("RelatedQuestionTypeId")]
+        public virtual QuestionType? RelatedQuestionType { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
