51d311c48c909d880c968f4f17da876da5e118b1
khoav <khoavnase183164@fpt.edu.vn>
2026-02-19 13:52:43 +0700

    feat: implement get overview result

---
+++ Tokki.Application/UseCases/UserExam/DTOs/UserExamResultResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokki.Application.UseCases.UserExam.DTOs
+{
+    public class UserExamResultResponse
+    {
+        public string UserExamId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string ExamTitle { get; set; } = string.Empty;
+
+        public SkillScoreDto Listening { get; set; } = new();
+        public SkillScoreDto Reading { get; set; } = new();
+        public SkillScoreDto Writing { get; set; } = new();
+
+        public double TotalScore => Listening.Score + Reading.Score + Writing.Score;
+    }
+
+    public class SkillScoreDto
+    {
+        public double Score { get; set; }
+        public double MaxScore { get; set; } 
+        public int CorrectAnswers { get; set; } 
+        public int TotalQuestions { get; set; }
+    }
+}
