d11a8ecea65efadf20fc49dd60d37b642c0f101b
khoav <khoavnase183164@fpt.edu.vn>
2026-02-20 13:28:04 +0700

    feat: implement publish wordle sentence

---
+++ Tokki.Application/UseCases/MiniGame/Commands/PublishWordleSentence/PublishWordleSentenceCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Tokki.Application.Common.Models;
+
+namespace Tokki.Application.UseCases.MiniGame.Commands.PublishWordleSentence
+{
+    public class PublishWordleSentenceCommand : IRequest<OperationResult<bool>>
+    {
+        [JsonIgnore]
+        public string UserId { get; set; } = string.Empty;
+
+        public string SubmissionId { get; set; } = string.Empty;
+        public bool IsPublic { get; set; }
+        public bool IsAnonymous { get; set; }
+    }
+}
---
+++ Tokki.Application/UseCases/MiniGame/Commands/PublishWordleSentence/PublishWordleSentenceCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokki.Application.UseCases.MiniGame.Commands.PublishWordleSentence
+{
+    public class PublishWordleSentenceCommandValidator : AbstractValidator<PublishWordleSentenceCommand>
+    {
+        public PublishWordleSentenceCommandValidator()
+        {
+            RuleFor(x => x.SubmissionId)
+                .NotEmpty().WithMessage("SubmissionId không được để trống.");
+        }
+    }
+}
