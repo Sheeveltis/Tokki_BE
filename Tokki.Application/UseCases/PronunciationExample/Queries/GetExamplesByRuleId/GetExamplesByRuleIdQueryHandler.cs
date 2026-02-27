c2a87f4901ab9fcd96e9f6a630c35d8db88f709d
khoav <khoavnase183164@fpt.edu.vn>
2026-02-27 10:14:31 +0700

    feat: implement get pronunciation example by rule id

---
+++ Tokki.Application/UseCases/PronunciationExample/Queries/GetExamplesByRuleId/GetExamplesByRuleIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tokki.Application.Common.Models;
+using Tokki.Application.IRepositories;
+using Tokki.Application.UseCases.PronunciationExample.DTOs;
+
+namespace Tokki.Application.UseCases.PronunciationExample.Queries.GetExamplesByRuleId
+{
+    public class GetExamplesByRuleIdQueryHandler : IRequestHandler<GetExamplesByRuleIdQuery, OperationResult<List<ExampleSimpleDTO>>>
+    {
+        private readonly IPronunciationExampleRepository _exampleRepository;
+
+        public GetExamplesByRuleIdQueryHandler(IPronunciationExampleRepository exampleRepository)
+        {
+            _exampleRepository = exampleRepository;
+        }
+
+        public async Task<OperationResult<List<ExampleSimpleDTO>>> Handle(GetExamplesByRuleIdQuery request, CancellationToken cancellationToken)
+        {
+            var examples = await _exampleRepository.GetExamplesByRuleIdAsync(request.PronunciationRuleId, cancellationToken);
+
+            var dtoList = examples.Select(e => new ExampleSimpleDTO
+            {
+                ExampleId = e.ExampleId,
+                RawScript = e.RawScript,
+                SortOrder = e.SortOrder
+            }).ToList();
+
+            return OperationResult<List<ExampleSimpleDTO>>.Success(dtoList, 200, "Lấy danh sách bài tập thành công.");
+        }
+    }
+}
