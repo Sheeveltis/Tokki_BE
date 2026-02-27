5de7539a1b656aa33e98cfc5b11d5349a498f1b5
khoav <khoavnase183164@fpt.edu.vn>
2026-02-27 10:14:06 +0700

    feat: implement get detail pronunciation by id

---
+++ Tokki.Application/UseCases/PronunciationExample/DTOs/ExampleDetailDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokki.Application.UseCases.PronunciationExample.DTOs
+{
+    public class ExampleDetailDTO
+    {
+        public string ExampleId { get; set; } = string.Empty;
+        public string TargetScript { get; set; } = string.Empty;
+        public string RawScript { get; set; } = string.Empty;
+        public string PhoneticScript { get; set; } = string.Empty;
+        public string Meaning { get; set; } = string.Empty;
+        public string AudioUrl { get; set; } = string.Empty;
+
+        public string RuleName { get; set; } = string.Empty;
+        public string RuleDescription { get; set; } = string.Empty;
+        public string RuleContent { get; set; } = string.Empty;
+    }
+}
