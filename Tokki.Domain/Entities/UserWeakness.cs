22cd6c52b44d368ec84a01007de61653cabd4f42
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-02-28 15:44:03 +0700

    feat: create UserWeakness

---
+++ Tokki.Domain/Entities/UserWeakness.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tokki.Domain.Entities
+{
+    public class UserWeakness
+    {
+        [Key]
+        public string Id { get; set; } = string.Empty;
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        public string QuestionTypeId { get; set; } = string.Empty;
+
+        public string? RoadmapId { get; set; }
+
+        public int Status { get; set; } 
+        public double? InitialScore { get; set; }
+        public double? CurrentScore { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        [ForeignKey("QuestionTypeId")]
+        public virtual QuestionType QuestionType { get; set; }
+    }
+}
