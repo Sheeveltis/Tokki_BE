470241a6c434be3c23833f8d0fc227a10c69cbc1
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-03-13 14:03:41 +0700

    feat: create RoadmapKnowledgeProfileRepository and IRoadmapKnowledgeProfileRepository

---
+++ Tokki.Application/IRepositories/IRoadmapKnowledgeProfileRepository.cs
@@ -0,0 +1,22 @@
+﻿using Tokki.Domain.Entities;
+
+namespace Tokki.Application.IRepositories
+{
+    public interface IRoadmapKnowledgeProfileRepository
+    {
+        Task<RoadmapKnowledgeProfile?> GetAsync(
+            string userRoadmapId,
+            string questionTypeId,
+            CancellationToken cancellationToken = default);
+
+        Task<List<RoadmapKnowledgeProfile>> GetByRoadmapIdAsync(
+            string userRoadmapId,
+            CancellationToken cancellationToken = default);
+
+        Task AddAsync(
+            RoadmapKnowledgeProfile profile,
+            CancellationToken cancellationToken = default);
+
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
