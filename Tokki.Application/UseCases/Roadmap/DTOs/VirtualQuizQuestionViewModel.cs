7688bda627019c98069ccb3237752799449d714f
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-03-13 09:41:47 +0700

    feat: create virtualQuizQuestion

---
+++ Tokki.Application/UseCases/Roadmap/DTOs/VirtualQuizQuestionViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Tokki.Application.UseCases.Roadmap.DTOs
+{
+    public class VirtualQuizQuestionViewModel
+    {
+        public string QuestionId { get; set; }
+        public string? Content { get; set; }
+        public string? MediaUrl { get; set; }
+        public string? PassageContent { get; set; }
+        public List<VirtualQuizOptionViewModel> Options { get; set; } = new();
+    }
+
+    public class VirtualQuizOptionViewModel
+    {
+        public string OptionId { get; set; }
+        public string KeyOption { get; set; }
+        public string? Content { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
