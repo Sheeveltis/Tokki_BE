db536c48b4bfc888098f0d7f6bcf73ba1eb6ab88
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-03-15 16:03:08 +0700

    refactor: add new enum pre-topikII

--- Tokki.Domain/Enums/CurrentTopikLevel.cs
+++ Tokki.Domain/Enums/CurrentTopikLevel.cs
@@ -4,8 +4,8 @@
     {
         Pre_Topik = 0, 
         Level_1 = 1,   
-        Level_2 = 2,   
-
+        Level_2 = 2,
+        Pre_Topik_II = 3,  
         Level_3 = 3,   
         Level_4 = 4,   
         Level_5 = 5,   
