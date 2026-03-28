d821a250ac9f999b20e353f9e18ba260e7f732ee
Sheeveltis <khoavo091003@gmail.com>
2026-03-28 15:54:09 +0700

    feat(pronunciationRule): implement get all

--- Tokki.Application/UseCases/PronunciationRule/Queries/GetPronunciationRules/GetPronunciationRulesQuery.cs
+++ Tokki.Application/UseCases/PronunciationRule/Queries/GetPronunciationRules/GetPronunciationRulesQuery.cs
@@ -1,4 +1,4 @@
-﻿using MediatR;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +9,10 @@ using Tokki.Application.UseCases.PronunciationRule.DTOs;
 
 namespace Tokki.Application.UseCases.PronunciationRule.Queries.GetPronunciationRules
 {
-    public class GetPronunciationRulesQuery : IRequest<OperationResult<List<PronunciationRuleDTO>>> { }
+    public class GetPronunciationRulesQuery : IRequest<OperationResult<PagedResult<PronunciationRuleDTO>>>
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+        public string? SearchTerm { get; set; }
+    }
 }
--- Tokki.WebAPI/Controllers/PronunciationRulesController.cs
+++ Tokki.WebAPI/Controllers/PronunciationRulesController.cs
@@ -1,8 +1,11 @@
-﻿using MediatR;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using Tokki.Application.UseCases.PronunciationRule.Commands.CreatePronunciationRule;
+using Tokki.Application.UseCases.PronunciationRule.Commands.DeletePronunciationRule;
+using Tokki.Application.UseCases.PronunciationRule.Commands.UpdatePronunciationRule;
+using Tokki.Application.UseCases.PronunciationRule.Queries.GetPronunciationRuleById;
 using Tokki.Application.UseCases.PronunciationRule.Queries.GetPronunciationRules;
 
 namespace Tokki.WebAPI.Controllers
@@ -16,35 +19,59 @@ namespace Tokki.WebAPI.Controllers
         {
             _sender = sender;
         }
+        [HttpGet]
+        public async Task<IActionResult> GetPronunciationRules([FromQuery] GetPronunciationRulesQuery query)
+        {
+            var result = await _sender.Send(query);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var result = await _sender.Send(new GetPronunciationRuleByIdQuery(id));
+            if (!result.IsSuccess)
+            {
+                return result.StatusCode == 404 ? NotFound(result) : BadRequest(result);
+            }
+            return Ok(result);
+        }
+
         [HttpPost]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin,Staff")]
         public async Task<IActionResult> CreateRule([FromBody] CreatePronunciationRuleCommand command)
         {
             var userId = User.FindFirst("UserId")?.Value
                         ?? User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            if (string.IsNullOrEmpty(userId))
-            {
-                return Unauthorized("Không xác định được người dùng.");
-            }
-            command.CreateBy = userId!;
-
+            command.CreateBy = userId;
             var result = await _sender.Send(command);
-
-            if (result.IsSuccess)
-            {
-                return Ok(result);
-            }
-
-            return BadRequest(result);
+            return result.IsSuccess ? Ok(result) : StatusCode(result.StatusCode, result);
         }
-        [HttpGet("rules")]
-        public async Task<IActionResult> GetPronunciationRules()
-        {
-            var query = new GetPronunciationRulesQuery();
-            var result = await _sender.Send(query);
 
-            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin,Staff")]
+        public async Task<IActionResult> UpdateRule(string id, [FromBody] UpdatePronunciationRuleCommand command)
+        {
+            var userId = User.FindFirst("UserId")?.Value
+                        ?? User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            command.PronunciationRuleId = id;
+            command.UpdateBy = userId;
+            var result = await _sender.Send(command);
+            return result.IsSuccess ? Ok(result) : StatusCode(result.StatusCode, result);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin,Staff")]
+        public async Task<IActionResult> DeleteRule(string id)
+        {
+            var result = await _sender.Send(new DeletePronunciationRuleCommand(id));
+            if (!result.IsSuccess)
+            {
+                return result.StatusCode == 404 ? NotFound(result) : BadRequest(result);
+            }
+            return Ok(result);
         }
     }
 }
