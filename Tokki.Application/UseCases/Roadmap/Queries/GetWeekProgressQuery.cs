d1b90a484ce324d03f58230cab34aa886cb4c4e9
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-04-02 10:43:10 +0700

    feat: create GetWeekProgressQuery

---
+++ Tokki.Application/UseCases/Roadmap/Queries/GetWeekProgressQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+
+namespace Tokki.Application.UseCases.Roadmap.Queries
+{
+    public class GetWeekProgressQuery : IRequest<OperationResult<int>>
+    {
+        public string RoadmapWeekId { get; set; } = string.Empty;
+    }
+}
