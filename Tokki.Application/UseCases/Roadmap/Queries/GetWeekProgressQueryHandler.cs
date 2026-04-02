2af096d849aa7bff1e3a84035669f69616cec7f5
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-04-02 10:49:53 +0700

    feat: create GetWeekProgressQueryHandler

---
+++ Tokki.Application/UseCases/Roadmap/Queries/GetWeekProgressQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+
+namespace Tokki.Application.UseCases.Roadmap.Queries
+{
+    public class GetWeekProgressQueryHandler : IRequestHandler<GetWeekProgressQuery, OperationResult<int>>
+    {
+        private readonly IUserRoadmapRepository _userRoadmapRepository;
+
+        public GetWeekProgressQueryHandler(IUserRoadmapRepository userRoadmapRepository)
+        {
+            _userRoadmapRepository = userRoadmapRepository;
+        }
+
+        public async Task<OperationResult<int>> Handle(GetWeekProgressQuery request, CancellationToken cancellationToken)
+        {
+            var week = await _userRoadmapRepository.GetWeekByIdAsync(request.RoadmapWeekId, cancellationToken);
+
+            if (week == null)
+                return OperationResult<int>.Failure("Không tìm thấy tuần học.", 404);
+
+            return OperationResult<int>.Success(week.ProgressPercent);
+        }
+    }
+}
