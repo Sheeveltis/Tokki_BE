132c2a7627d3dbb7a49327fd079d981b75625bdd
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-04-23 01:23:56 +0700

    feat: create TopikLevelConfigDto

---
+++ Tokki.Application/UseCases/Roadmap/DTOs/TopikLevelConfigDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Tokki.Application.UseCases.Roadmap.DTOs
+{
+    public class TopikLevelConfigDto
+    {
+        public int TargetAimLevel { get; set; }
+        public int PassScore { get; set; }
+        public int TotalScore { get; set; }
+        public string ExamGroup { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+        public int EntranceExamType { get; set; }
+        public string ConfigKey { get; set; } = string.Empty;
+    }
+}
