fc2b16840201ea91299157a0574f38eabfa8c566
hna-gnart <anhtntse183272@fpt.edu.vn>
2026-05-04 10:13:52 +0700

    feat: create CancelPaymentCommand

---
+++ Tokki.Application/UseCases/Payments/Commands/CreatePayment/CancelPaymentCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Tokki.Application.Common.Models;
+
+namespace Tokki.Application.UseCases.Payments.Commands.CreatePayment
+{
+    public class CancelPaymentCommand : IRequest<OperationResult<string>>
+    {
+        public string PaymentId { get; set; }
+    }
+}
